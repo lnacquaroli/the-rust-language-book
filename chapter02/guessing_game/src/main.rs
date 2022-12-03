7847ecbc5b62022e403f65422f7a412fcb201de9
Leandro Acquaroli <>
2022-12-03 15:01:09 -0500

    Added comment to chapter 02

--- chapter02/guessing_game/src/main.rs
+++ chapter02/guessing_game/src/main.rs
@@ -3,6 +3,7 @@
 // We’ll implement a classic beginner programming problem: a guessing game. Here’s how it works: the program will generate a random integer between 1 and 100. It will then prompt the player to enter a guess. After a guess is entered, the program will indicate whether the guess is too low or too high. If the guess is correct, the game will print a congratulatory message and exit.
 
 // Crates.io, seach rand and copy the info into the Cargo.toml file
+// Then cargo build will deal with the deps
 
 // Bring the input/output library into scope from the standard library
 use rand::Rng;
